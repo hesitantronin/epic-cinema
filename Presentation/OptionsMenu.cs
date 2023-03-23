9bd8ceed6f72c1b98a4c717805507da5e3050c4d
hesitantronin <roninvanegdom@outlook.com>
2023-03-23 13:29:00 +0100

    Files aangepast naar ons klassendiagram

--- Presentation/OptionsMenu.cs
+++ Presentation/OptionsMenu.cs
@@ -1,4 +1,4 @@
-static class Menu
+static class OptionsMenu
 {
 
     //This shows the menu. You can call back to this method to show the menu again
--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to this amazing program");
-Menu.Start();
+﻿
+public class program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Welcome to this amazing program");
+        OptionsMenu.Start();
+    }
+}
+
