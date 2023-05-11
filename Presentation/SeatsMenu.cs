2f868891d298b3b0fa2d7ab1d03aab1448e1b13c
GhostOfIcarus <alysha.van.etten@hotmail.nl>
2023-05-11 13:04:32 +0200

    he a seat legend

---
+++ Presentation/SeatsMenu.cs
@@ -0,0 +1,30 @@
+static class SeatMenu
+{
+    public static void SeatLegend()
+    {                        
+        Console.ForegroundColor = ConsoleColor.DarkGray;
+        Console.Write("■");
+        Console.ResetColor();
+        Console.Write(" - UNAVAILABLE\n");
+
+        Console.ForegroundColor = ConsoleColor.DarkCyan;
+        Console.Write("■");
+        Console.ResetColor();
+        Console.Write(" - PRICERANGE 1\n");
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write("■");
+        Console.ResetColor();
+        Console.Write(" - PRICERANGE 2\n");
+
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.Write("■");
+        Console.ResetColor();
+        Console.Write(" - PRICERANGE 3\n");
+
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.Write("■");
+        Console.ResetColor();
+        Console.Write(" - SELECTED SEATS\n");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,23 +2,8 @@
 {
     public static void Main()
     {
-        OptionsMenu.Start();
+        // OptionsMenu.Start();
 
-
-        //string[][] data = SeatAccess.LoadAuditorium();
-        //SeatAccess.PrintAuditorium(data);
-
-        //SeatAccess.UpdateSeatValue(data, "C1", "0");
-
-        //SeatAccess.PrintAuditorium(data);
-
-        //MovieModel movie = new(1234);
-
-        //string[][] beh = SeatAccess.LoadAuditorium();
-
-        //SeatAccess.UpdateSeatValue(beh, "C1", "bep");
-
-
-        //SeatAccess.WriteToCSV(beh, SeatAccess.NewAuditorium(movie));
+        SeatMenu.SeatLegend();
     }
 }
