f8a5699f14d1b7408d5f54375342db6e9f319291
rosalie-gr <124690847+rosalie-gr@users.noreply.github.com>
2023-05-22 11:51:39 +0200

    Reserving as a guest makes you create an account

--- Presentation/ReservationMenu.cs
+++ Presentation/ReservationMenu.cs
@@ -4,6 +4,107 @@ class ReservationMenu
 {
     static public void Start()
     {
+        AccountsLogic accountsLogic = new AccountsLogic();
+
+        if (AccountsLogic.CurrentAccount.Type == AccountModel.AccountType.GUEST)
+        {
+            Console.Clear();
+            OptionsMenu.Logo("registration");
+            Console.WriteLine("In order to finalize your reservation, please create an account.");
+            
+            Console.CursorVisible = true;
+            string email = "";
+
+            while(true)
+            {
+                Console.WriteLine("Email Address:");
+                email = Console.ReadLine() + "";
+
+                 if (!accountsLogic.IsEmailValid(email))
+                {
+                    List<string> EList = new List<string>(){"Continue"};
+
+                    int option = OptionsMenu.DisplaySystem(EList, "", "\nInvalid email, please try again.", false, true);
+
+                    if (option == 2)
+                    {
+                        return;
+                    }
+                }
+                else if(accountsLogic.IsEmailInUse(email))
+                {
+                    List<string> EList = new List<string>(){"Continue"};
+
+                    int option = OptionsMenu.DisplaySystem(EList, "", "\nThis email is already in use, please try again.", false, true);
+
+                    if (option == 2)
+                    {
+                        return;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            string password = "";
+            string confirmedPassword = "no match";
+
+            while (password != confirmedPassword)
+            {
+                Console.Clear();
+
+                OptionsMenu.Logo("registration");
+                Console.WriteLine("Password:");
+
+                password = accountsLogic.GetMaskedPassword();
+                if (accountsLogic.IsPasswordValid(password))
+                {
+                    Console.Clear();
+                    OptionsMenu.Logo("registration");
+
+                    Console.WriteLine("Confirm Password:");
+                    confirmedPassword = accountsLogic.GetMaskedPassword();
+
+                    if (password != confirmedPassword)
+                    {
+                        List<string> BList = new List<string>() { "Continue" };
+
+                        int option = OptionsMenu.DisplaySystem(BList, "", "\nPasswords do not match, please try again.", false, true);
+
+                        if (option == 2)
+                        {
+                            return;
+                        }
+                    }
+                }
+                else
+                {
+                    List<string> CList = new List<string>() { "Continue" };
+                    int option = OptionsMenu.DisplaySystem(CList, "", "\nPassword must be between 8 and 32 characters long and contain atleast one number, one capital letter and one special character", false, true);
+                
+                    if (option == 2)
+                    {
+                        return;
+                    }
+                }
+            }
+
+            Console.Clear();
+            OptionsMenu.Logo("Registration");
+            Console.WriteLine("Please enter your name:");
+            string fullName = Console.ReadLine() + "";
+
+            AccountsLogic.CurrentAccount.EmailAddress = email;
+            AccountsLogic.CurrentAccount.Password = accountsLogic.HashPassword(password);
+            AccountsLogic.CurrentAccount.FullName = fullName;
+            AccountsLogic.CurrentAccount.Type = AccountModel.AccountType.CUSTOMER;
+            accountsLogic.UpdateList(AccountsLogic.CurrentAccount);
+        }
+        
+        AccountsLogic accountslogic = new AccountsLogic();
+
         List<string> ReturnList = new List<string>()
         {
             "Yes",
@@ -21,9 +122,8 @@ class ReservationMenu
 
                 if (accessibilityReq != null)
                 {
-                    AccountsLogic accountslogic = new AccountsLogic();
                     AccountsLogic.CurrentAccount.AccessibilityRequest = accessibilityReq;
-                    accountslogic.UpdateList(AccountsLogic.CurrentAccount);
+                    accountsLogic.UpdateList(AccountsLogic.CurrentAccount);
                 }
                 break;
             
